a34136c84d29aa4ce8c1f35d904c94a8166ee344
Akemmanuel <akemsoft@gmail.com>
2025-11-26 07:59:45 -0300

    Remove spinning React logo from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,15 @@ import { APITester } from "./APITester";
 import "./index.css";
 
 import logo from "./logo.svg";
-import reactLogo from "./react.svg";
 
 export function App() {
   return (
     <div className="container mx-auto p-8 text-center relative z-10">
-      <div className="flex justify-center items-center gap-8 mb-8">
+      <div className="flex justify-center items-center mb-8">
         <img
           src={logo}
           alt="Bun Logo"
           className="h-36 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#646cffaa] scale-120"
-        />
-        <img
-          src={reactLogo}
-          alt="React Logo"
-          className="h-36 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#61dafbaa] [animation:spin_20s_linear_infinite]"
         />
       </div>
       <Card>
